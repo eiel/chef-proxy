9d8bf5e769e4541d2764d4eed5ff7b67174611f1
Tomohiko Himura <eiel.hal@gmail.com>
2014-04-25 15:51:23 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# これは何
+
+squid の動作確認がしたかっただけの cookbook
+
+
+```
+$ cd example
+$ bundle install
+$ libralian
+$ vagrant up
+```
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'proxy'
+maintainer       'Tomohiko Himura'
+maintainer_email 'eiel.hal@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures proxy'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+dependedepends   'squid'
